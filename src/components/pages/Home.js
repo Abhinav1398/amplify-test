2d601d7d99859c696c38eb47a630fbb6759390d3
abhdubey2 <abhdubey2@publicisgroupe.net>
2024-05-05 12:29:50 +0530

    feat: added header pages router and footer

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import '../../App.scss';
+import { Cards } from '../Cards';
+import HeroSection from '../HeroSection/HeroSection';
+
+function Home () {
+    return (
+        <>
+            <HeroSection />
+            <Cards />
+        </>
+    )
+}
+
+export default Home;
---
+++ src/components/pages/Services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.scss';
+
+function Services () {
+    return (
+        <h1 className='services'>
+            Services
+        </h1>
+    )
+}
+
+export default Services;
